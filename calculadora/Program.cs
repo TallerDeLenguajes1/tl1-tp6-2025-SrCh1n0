ec9ea95ebb3d21471f61f5310f901d885b6a5c20
SrCh1n0 <facundo.lopez_hd@hotmail.com>
2025-06-24 23:06:52 -0300

    Creacion de la consola y agregada la opracion suma

---
+++ calculadora/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+int operacion = -1;
+bool valido;
+
+do
+{
+    Console.WriteLine("Ingrese operación a realizar:");
+    Console.WriteLine("1. Suma" + $"2. Resta" + $"3. Multiplicación" + $"4. División");
+
+    string? opcion = Console.ReadLine();
+    valido = int.TryParse(opcion, out operacion);
+
+    if (!valido)
+    {
+        Console.WriteLine("Ingrese un número válido.");
+        continue;
+    }
+    
+    switch (operacion)
+    {
+        case 1:
+            Console.WriteLine("Suma");
+            Console.Write("Ingrese el primer número: ");
+            string? input1 = Console.ReadLine();
+            Console.Write("Ingrese el segundo número: ");
+            string? input2 = Console.ReadLine();
+
+            if (int.TryParse(input1, out int num1) && int.TryParse(input2, out int num2))
+            {
+                int resultado = num1 + num2;
+                Console.WriteLine($"El resultado de la suma es: {resultado}");
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida.");
+            }
+            break;
+
+        default:
+            break;
+    }
+} while (true);
