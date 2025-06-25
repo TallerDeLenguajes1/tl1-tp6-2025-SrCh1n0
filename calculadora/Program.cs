a7abc4c8ac4eb68f0c31b4cfa673523c54b63143
SrCh1n0 <facundo.lopez_hd@hotmail.com>
2025-06-24 23:21:54 -0300

    Agrego la opracion resta

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -4,39 +4,57 @@ bool valido;
 
 do
 {
-    Console.WriteLine("Ingrese operación a realizar:");
-    Console.WriteLine("1. Suma" + $"2. Resta" + $"3. Multiplicación" + $"4. División");
+    Console.WriteLine($"Ingrese operación a realizar:");
+    Console.WriteLine($"1. Suma \n2. Resta \n3. Multiplicación \n4. División");
 
     string? opcion = Console.ReadLine();
     valido = int.TryParse(opcion, out operacion);
 
     if (!valido)
     {
-        Console.WriteLine("Ingrese un número válido.");
+        Console.WriteLine($"Ingrese un número válido.");
         continue;
     }
-    
+    int num1, num2;
+    string? input1, input2;
     switch (operacion)
     {
         case 1:
             Console.WriteLine("Suma");
             Console.Write("Ingrese el primer número: ");
-            string? input1 = Console.ReadLine();
+            input1 = Console.ReadLine();
             Console.Write("Ingrese el segundo número: ");
-            string? input2 = Console.ReadLine();
+            input2 = Console.ReadLine();
 
-            if (int.TryParse(input1, out int num1) && int.TryParse(input2, out int num2))
+            if (int.TryParse(input1, out num1) && int.TryParse(input2, out num2))
             {
                 int resultado = num1 + num2;
                 Console.WriteLine($"El resultado de la suma es: {resultado}");
             }
             else
             {
+                Console.WriteLine($"Entrada inválida.");
+            }
+            break;
+        case 2:
+            Console.WriteLine("Resta");
+            Console.Write("Ingrese el primer número: ");
+            input1 = Console.ReadLine();
+            Console.Write("Ingrese el segundo número: ");
+            input2 = Console.ReadLine();
+
+            if (int.TryParse(input1, out num1) && int.TryParse(input2, out num2))
+            {
+                int resultado = num1 - num2;
+                Console.WriteLine($"El resultado de la resta es: {resultado}");
+            }
+            else
+            {
                 Console.WriteLine("Entrada inválida.");
             }
             break;
-
-        default:
+        case 0:
+            Console.WriteLine("Cerrando consola.");
             break;
     }
-} while (true);
+} while (operacion != 0);
