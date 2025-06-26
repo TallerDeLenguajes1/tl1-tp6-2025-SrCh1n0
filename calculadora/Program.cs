94f0c09a8c09df90de6f62427fe7cef748fd614e
SrCh1n0 <facundo.lopez_hd@hotmail.com>
2025-06-25 23:16:56 -0300

    Agrego la operacion cuadrado

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -1,17 +1,16 @@
-﻿int operacion = -1;
+﻿int operacion = -1, num, resultado ;
 bool valido;
-
+string? input;
 do
 {
     Console.WriteLine("Ingrese operación a realizar:");
     Console.WriteLine("1. Valor Absoluto \n2. Cuadrado \n3. Raiz Cuadrada \n4. Seno \n5. Coseno \n6.Parte Entera \n0. Salir");
     string? opcion = Console.ReadLine();
     valido = int.TryParse(opcion, out operacion);
-
+    
     if (!valido)
     {
         Console.WriteLine("Ingrese un número válido.");
-        continue;
     }
 
     switch (operacion)
@@ -19,12 +18,27 @@ do
         case 1:
             Console.WriteLine("Valor Absoluto");
             Console.Write("Ingrese un número: ");
-            string? input = Console.ReadLine();
+            input = Console.ReadLine();
 
-            if (int.TryParse(input, out int num))
+            if (int.TryParse(input, out num))
             {
-                int resultado = (num < 0) ? -num : num;
+                resultado = (num < 0) ? -num : num;
                 Console.WriteLine($"El valor absoluto es: {resultado}");
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida.");
+            }
+            break;
+        
+        case 2:
+            Console.WriteLine("Cuadrado");
+            Console.Write("Ingrese un número: ");
+            input = Console.ReadLine();
+            if (int.TryParse(input, out num))
+            {
+                resultado = num * num;
+                Console.WriteLine($"El resultado del cuadrado es: {resultado}");
             }
             else
             {
