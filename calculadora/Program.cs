246858d2749d11eb7c9fc1f2117351e6ab6699fa
SrCh1n0 <facundo.lopez_hd@hotmail.com>
2025-06-25 23:07:24 -0300

    Inicio la calculadora y agrego la operacion valor absoluto

---
+++ calculadora/Program.cs
@@ -0,0 +1,39 @@
+﻿int operacion = -1;
+bool valido;
+
+do
+{
+    Console.WriteLine("Ingrese operación a realizar:");
+    Console.WriteLine("1. Valor Absoluto \n2. Cuadrado \n3. Raiz Cuadrada \n4. Seno \n5. Coseno \n6.Parte Entera \n0. Salir");
+    string? opcion = Console.ReadLine();
+    valido = int.TryParse(opcion, out operacion);
+
+    if (!valido)
+    {
+        Console.WriteLine("Ingrese un número válido.");
+        continue;
+    }
+
+    switch (operacion)
+    {
+        case 1:
+            Console.WriteLine("Valor Absoluto");
+            Console.Write("Ingrese un número: ");
+            string? input = Console.ReadLine();
+
+            if (int.TryParse(input, out int num))
+            {
+                int resultado = (num < 0) ? -num : num;
+                Console.WriteLine($"El valor absoluto es: {resultado}");
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida.");
+            }
+            break;
+        
+        case 0:
+            Console.WriteLine("Cerrando calculadora.");
+            break;
+    }
+} while (operacion != 0);
